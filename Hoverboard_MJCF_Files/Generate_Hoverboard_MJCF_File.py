548e7350c794474d8961d1f630e5a346dc366484
Shreyas Kulkarni <shreyask808@gmail.com>
2026-08-26 22:49:38 -0400

    Create Generate_Hoverboard_MJCF_File.py

---
+++ Hoverboard_MJCF_Files/Generate_Hoverboard_MJCF_File.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+
+#=================================================================================================================================================================================
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#=================================================================================================================================================================================
+# Hoverboard Dimensions
+R = float(input("Enter Hoverboard Wheel Radius R [m]:"))
+L = float(input("Enter the Hoverboard Wheelbase L [m]:"))
+M = float(input("Enter Hoverboard Wheel Mass M [kg]:"))
+h = float(input("Enter Inverted Pendulum Length h [m]:"))
+m = float(input("Enter Inverted Pendulum Mass m [kg]:"))
+
