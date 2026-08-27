b04ec87631f87d0c847c530e1e46976153c22d11
Shreyas Kulkarni <shreyask808@gmail.com>
2026-08-26 23:51:50 -0400

    Update Generate_Hoverboard_MJCF_File.py

--- Hoverboard_MJCF_Files/Generate_Hoverboard_MJCF_File.py
+++ Hoverboard_MJCF_Files/Generate_Hoverboard_MJCF_File.py
@@ -13,3 +13,19 @@ M = float(input("Enter Hoverboard Wheel Mass M [kg]:"))
 h = float(input("Enter Inverted Pendulum Length h [m]:"))
 m = float(input("Enter Inverted Pendulum Mass m [kg]:"))
 
+#=================================================================================================================================================================================
+# Generate MJCF File
+def generate_MJCF(R,L,M,h,m):
+    return f""" <mujoco model="hoverboard">
+    <option gravity="0 0 -9.81" timestep="0.002" integrator="RK4"/>
+
+    <default>
+        <joint damping="0.01"/>
+        <geom friction="1.0 0.005 0.0001"/>
+    </default>
+
+    <worldbody>
+        <geom name="floor" type="plane" size="5 5 0.1" rgba="0.8 0.8 0.8 1"/>
+        
+    </worldbody>
+    """
