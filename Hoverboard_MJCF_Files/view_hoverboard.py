a485bff09192b6cd692abc5567663e90abe56ea1
Shreyas Kulkarni <shreyask808@gmail.com>
2026-09-02 16:41:01 -0400

    Update view_hoverboard.py

--- Hoverboard_MJCF_Files/view_hoverboard.py
+++ Hoverboard_MJCF_Files/view_hoverboard.py
@@ -1,12 +1,14 @@
 import mujoco
 import mujoco.viewer
+import numpy as np
 
 xml_path = "/mnt/c/Users/Admin/Documents/Github/Hoverboard_RL_Controls/Hoverboard_MJCF_Files/hoverboard_1.xml"
 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
 
-with mujoco.viewer.launch_passive(model, data) as viewer:
-    while viewer.is_running():
-        mujoco.mj_step(model, data)
-        viewer.sync()
+duration = 5
+framerate = 60
+
+while data.time < duration:
+    mujoco.mj_step(model, data)
