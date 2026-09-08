a5fe51b3f61216fc282e8b6474d745582543fea6
Shreyas Kulkarni <shreyask808@gmail.com>
2026-09-07 22:33:56 -0400

    Update Mujoco_Rollout_Trial.py

--- Hoverboard_PPO_Training/Mujoco_Rollout_Trial.py
+++ Hoverboard_PPO_Training/Mujoco_Rollout_Trial.py
@@ -1,11 +1,25 @@
 import mujoco
 import mujoco.viewer
+import gymnasium
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 
 xml_path = "/mnt/c/Users/admin/Documents/Github/Hoverboard_RL_Controls/Hoverboard_MJCF_Files/hoverboard_3.xml"
 
-hboard_model = mujoco.MjModel.from_xml_path(xml_path)
-hboard_data = mujoco.MjData(hboard_model)
+## Gymnasium Class
+class hoverboardEnv(gymnasium.Env):
+    def __init__(self,xml_path):
+        self.hbmodel = mujoco.MjModel.from_xml_path(xml_path)
+        self.hbdata = mujoco.MjData(self.hbmodel)
+        obs_dim = self.hbdata.nq + self.hbdata.nv
+        ctrl_dim = self.hbdata.nu
+        self.observation_space = gymnasium.spaces.Box(
+            low= -np.inf, high= np.inf, shape=(obs_dim,), dtype=np.float32
+        )
+        self.action_space = gymnasium.spaces.Box(
+            low=-self.hbmodel.actuator.ctrlrange[:,0],
+            high= self.hbmodel.actuator.ctrlrange[:,1],
+            shape= (ctrl_dim,), dtype=np.float32
+        )
 
