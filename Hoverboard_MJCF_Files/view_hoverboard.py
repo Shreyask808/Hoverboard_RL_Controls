5810f3a3b2536b934a21be1e37e1299c1972f020
Shreyas Kulkarni <shreyask808@gmail.com>
2026-09-13 22:22:56 -0400

    Update view_hoverboard.py

--- Hoverboard_MJCF_Files/view_hoverboard.py
+++ Hoverboard_MJCF_Files/view_hoverboard.py
@@ -3,11 +3,34 @@ import mujoco.viewer
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
 
 xml_path = "/mnt/c/Users/Admin/Documents/Github/Hoverboard_RL_Controls/Hoverboard_MJCF_Files/hoverboard_3.xml"
+weights = "/mnt/c/Users/Admin/Documents/Github/Hoverboard_RL_Controls/Vanilla_PG_Implementation/attempt_1.pth"
 
 model = mujoco.MjModel.from_xml_path(xml_path)
 data = mujoco.MjData(model)
+
+class PolicyNet(nn.Module):
+    def __init__(self, in_dim, l1_dim, l2_dim, out_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(in_dim, l1_dim)
+        self.fc2 = nn.Linear(l1_dim, l2_dim)
+        self.fc3 = nn.Linear(l2_dim, out_dim)
+        self.log_std = nn.Parameter(torch.zeros(out_dim))
+
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mean = torch.tanh(self.fc3(x)) # Continuous Gaussian Distribution 
+        std = torch.exp(self.log_std)
+        return mean, std
 
 time_log = []
 theta = []
@@ -16,6 +39,10 @@ gamma =[]
 gammadot = []
 Ml_log = []
 Mr_log = []
+
+input_dim = (model.nq + model.nv)                   
+output_dim = model.nu
+nn_policy = PolicyNet(input_dim,64,64,output_dim)   
 
 duration= 100
 left_motor_id = model.actuator("left_motor").id
@@ -26,21 +53,23 @@ pend_hinge_y_qpos_id = model.joint("pend_hinge_y").qposadr[0]
 pend_hinge_x_qvel_id = model.joint("pend_hinge_x").dofadr[0]
 pend_hinge_y_qvel_id = model.joint("pend_hinge_y").dofadr[0]
 
-data.qpos[pend_hinge_y_qpos_id] = np.deg2rad(10)
+data.qpos[pend_hinge_y_qpos_id] = np.deg2rad(0)
 mujoco.mj_forward(model,data)
 
-T_max = model.actuator("left_motor").ctrlrange[1]
+low = model.actuator_ctrlrange[:,0]
+high = model.actuator_ctrlrange[:,1]
 
 with mujoco.viewer.launch_passive(model,data) as viewer:
     while viewer.is_running() and data.time < duration:
         step_start = time.time()
 
-        Ml = -3*data.qpos[pend_hinge_y_qpos_id] - 2*data.qpos[pend_hinge_x_qpos_id] + 0.1*data.qvel[pend_hinge_y_qvel_id] #- 0.1*data.qvel[pend_hinge_x_qvel_id]
-        Mr = -3*data.qpos[pend_hinge_y_qpos_id] + 2*data.qpos[pend_hinge_x_qpos_id] + 0.1*data.qvel[pend_hinge_y_qvel_id] #+ 0.1*data.qvel[pend_hinge_x_qvel_id]
-
-        Ml = np.clip(Ml,-T_max,T_max)
-        Mr = np.clip(Mr,-T_max,T_max)
-
+        states = np.concatenate([data.qpos,data.qvel])
+        inputs = torch.tensor(states, dtype=torch.float32).unsqueeze(0) 
+        mean, std = nn_policy(inputs)
+        action = mean.squeeze(0).detach().numpy()
+        torques = low + (action + 1)*(high - low)/2
+        Ml = torques[0]
+        Mr = torques[1]
         data.ctrl[left_motor_id] = Ml
         data.ctrl[right_motor_id] = Mr
 
@@ -90,8 +119,8 @@ plt.show()
 fig, (ax1) = plt.subplots(1,1, figsize=[10,12], sharex=True)
 ax1.plot(time_log,Ml_log,color='blue',label='Left Wheel Torque')
 ax1.plot(time_log,Mr_log,color='red',label='Right Wheel Torque')
-ax1.axhline(-T_max,color='black',linestyle='--')
-ax1.axhline(T_max,color='black',linestyle='--')
+#ax1.axhline(-T_max,color='black',linestyle='--')
+#ax1.axhline(T_max,color='black',linestyle='--')
 ax1.grid(True, alpha=0.3)
 ax1.set_xlabel('Time [sec]')
 ax1.set_ylabel('Motor Torque [N.m]')
